c5d1102a9c382f78a2cbdc87b49d2743004b30fb
Swapnil Negi <swapnil.negi09@gmail.com>
2017-07-01 20:16:42 +0530

    added images and tabs

--- app.js
+++ app.js
@@ -4,18 +4,30 @@
 		this.products = gems;
 	});
 
+	app.controller("PanelController", function(){
+		this.tab = 1;
+		this.selectTab = function(setTab){
+			this.tab = setTab;
+		};
+		this.isSelected = function(checkTab){
+			return this.tab === checkTab;
+		};
+	});
+
 	var gems = [
 		{
 			name: 'Swap',
 			price: 100,
 			description: 'extremely awesome',
 			canPurchase: true,
+			images: 'img1.jpg',
 		},
 		{
 			name: 'Sal',
 			price: 150,
 			description: 'magnificent',
 			canPurchase: false,
+			images: 'img2.jpg',
 		}
 	]
 })();
